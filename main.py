33cf306af72b456b8178b7becfc4833b4bb9b423
kebbaps <132104120+kebbaps@users.noreply.github.com>
2023-05-08 03:16:49 +0000

    changed questions

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-filename = input("What is your file name: ")
-name = input("What is your name: ")
+filename = input("Enter your file name: ")
+name = input("Enter your name: ")
 address = input("What is your street address: ")
 phone = input("What is your phone number: ")
 
