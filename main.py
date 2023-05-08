5d3c21001c44997147edf256efcf86423705d3f2
kebbaps <132104120+kebbaps@users.noreply.github.com>
2023-05-08 03:19:55 +0000

    github test

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 filename = input("Enter your file name: ")
 name = input("Enter your name: ")
 address = input("What is your street address: ")
-phone = input("What is your phone number: ")
+phone = input("Enter your phone number: ")
 
 with open(filename, 'w') as file:
     file.write(f"{name},{address},{phone}")
